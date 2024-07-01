8c6d6c88be29f4a27437eb552ef7e5e159eadd4a
Sakshi <sakshi6686.g@gmail.com>
2024-07-02 00:11:28 +0530

    Initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+ 
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+        <main>
+           <p className="text-white-1">Left SideBar</p>
+          {children}
+            <p className="text-white-1">Right SideBar</p>
+        </main>
+    </div>
+  );
+}
